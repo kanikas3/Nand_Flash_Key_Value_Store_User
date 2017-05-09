0f73a0d2ce8a79c43f981d2643ed5aafa560e2ed
Kanika Sabharwal <kanikas3@vt.edu>
2017-05-09 12:53:21 -0400

    Add help menu in tesh.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+	echo "Illegal number of parameters"
+	echo "./test.sh <test_number> <num_blocks> <is_large_key>"
+	echo "test_number: "
+	echo "    1 for read-write test"
+	echo "    2 for delete test"
+	echo "    3 for update test"
+	echo "num_blocks: "
+	echo "    count of the number of blocks"
+	echo "is_large_key: "
+	echo "    1 for key/val more than page size"
+	echo "    0 for key/val less than page size"
+	exit 0
+fi
+
 rmmod project6
 rmmod nandsim
 
