248fdaccdec56aad94b1817c11d24cc3ae41a87d
Kanika Sabharwal <kanikas3@vt.edu>
2017-05-10 13:51:50 -0400

    Update the scripts

--- short_test.sh
+++ short_test.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 if [ "$#" -ne 3 ]; then
+	echo :"DONOT USE THIS FOR HUGE PAGES, OTHERWISE IT WILL SPAM STDOUT(USE TEST.SH INSTEAD"
 	echo "Illegal number of parameters"
-	echo "./test.sh <test_number> <num_blocks> <is_large_key>"
+	echo "./short_test.sh <test_number> <num_pages> <is_large_key>"
 	echo "test_number: "
 	echo "    1 for read-write test"
 	echo "    2 for delete test"
 	echo "    3 for update test"
 	echo "    4 for remount test"
-	echo "num_blocks: "
-	echo "    count of the number of blocks"
+	echo "num_pages: "
+	echo "    count of the number of pages"
 	echo "is_large_key: "
 	echo "    1 for key/val more than page size"
 	echo "    0 for key/val less than page size"
@@ -19,27 +20,21 @@ fi
 rmmod project6
 rmmod nandsim
 
-COUNTER=$2
-
-./launch_flash_simulator.sh $COUNTER
+./launch_flash_simulator.sh 5
 ./insert_mod.sh
 
-let sum=64
-let value=$COUNTER*$sum
-let value=value-1
-
-echo "######## Running for $COUNTER blocks #########"
+echo "######## Running for $2 pages #########"
 
 
 if [ "$1" -ne 4 ]; then
-	./testbench $1 $value $3 0
+	./testbench $1 $2 $3 1
 fi
 
 
 if [ "$1" -eq 4 ]; then
-	./testbench 5 $value $3 0
+	./testbench 5 $2 $3 1
 	./insert_mod.sh
-	./testbench 6 $value $3 0
+	./testbench 6 $2 $3 1
 fi
 
 echo "#################################################"
